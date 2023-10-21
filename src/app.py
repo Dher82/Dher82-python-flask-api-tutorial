b6f220d7629f9608260115a1d41ac77111302e80
Dher82 <126974687+Dher82@users.noreply.github.com>
2023-10-21 16:46:09 +0000

    py-flask-tutorial_Dave

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
 from flask import request
 
-
 app = Flask(__name__)
 
 todos = [
@@ -14,25 +13,27 @@ def hello_world():
     json_text = jsonify(todos)
     return json_text
 
-
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.json
-    if request_body:
-       new_todo={
-          "label": request_body.get("label", ""),
-          "done":request_body.get("done", False)
-       }
-       todos.append(new_todo)
-       return jsonify(todos), 201
-    else:
-       return "Invalid request body", 400
+    try:
+        new_todo = request.get_json()
+        todos.append(new_todo)
+        return jsonify(todos)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
     
-@app.route('/todos/<int:position>', methods=['DELETE'])
-def delete_todo(position):
-    print("This is the position to delete: ",position)
-    return 'something'
-
+@app.route('/todos/<int:position_id>', methods=['DELETE'])
+def delete_todo(position_id):
+    try:
+        
+        if position_id >= 0 and position_id < len(todos):
+            
+            todos.pop(position_id)
+            return jsonify(todos)
+        else:
+            return jsonify({"error": "Position out of range"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 
 
